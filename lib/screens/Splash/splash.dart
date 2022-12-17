cebb427be4a22d0d69fb9580042f0e704422a1b4
azrael3199 <chinmayghungurde@gmail.com>
2022-12-17 22:49:47 +0530

    chore: format files

--- lib/screens/Splash/splash.dart
+++ lib/screens/Splash/splash.dart
@@ -7,32 +7,30 @@ import 'package:flutter/material.dart';
 ///
 
 class SplashTransition extends StatefulWidget {
-  const SplashTransition({super.key });
+  const SplashTransition({super.key});
 
   @override
   State<SplashTransition> createState() => _SplashTransitionState();
 }
 
 class _SplashTransitionState extends State<SplashTransition> {
-
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () => Navigator.pushReplacement(context, MaterialPageRoute(builder:
-                                                          (context) => 
-                                                          const SecondScreen()
-                                                         ))
-                                                        );
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const SecondScreen())));
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Image.asset('assets/images/logo_main.png', height: MediaQuery.of(context).size.height),
+      child: Image.asset('assets/images/logo_main.png',
+          height: MediaQuery.of(context).size.height),
     );
   }
-
 }
 
 class SecondScreen extends StatelessWidget {
@@ -43,8 +41,10 @@ class SecondScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Home")),
       body: const Center(
-        child:Text("Home page",textScaleFactor: 2,)
-      ),
+          child: Text(
+        "Home page",
+        textScaleFactor: 2,
+      )),
     );
   }
 }
